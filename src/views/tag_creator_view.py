2acd81f982c46f56315c4cee8a44052251e4898b
Hugo Alexandre Cavalcanti Porto <hugo.cavalcanti@altbank.co>
2024-02-14 11:37:14 -0300

    feat create error handler

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,9 +1,11 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorControler
 
 class TagCreatorView:
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorControler()
         body = http_request.body
         product_code = body['product_code']
-
-        return HttpResponse(status_code=200, body={'tag': f'{product_code}-tag'})
+        response = tag_creator_controller.create(product_code)
+        return HttpResponse(status_code=201, body=response)
